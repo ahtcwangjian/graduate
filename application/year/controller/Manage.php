b17e8bb1bc95927e7019b2011a6eca709b5fa4c6
ahtcwangjian <ahtcwangjian@qq.com>
2018-05-10 10:50:00 +0800

    修改身份功能

--- application/year/controller/Manage.php
+++ application/year/controller/Manage.php
@@ -4,7 +4,7 @@
  * @Author: wangjian
  * @Date:   2018-05-08 22:26:09
  * @Last Modified by:   wangjan
- * @Last Modified time: 2018-05-08 22:26:52
+ * @Last Modified time: 2018-05-10 10:42:12
  * 用户操作年鉴信息的界面
  */
 namespace app\year\controller;
@@ -15,5 +15,16 @@ class Manage extends Controller
     public function index()
     {
         return $this->fetch();
+    }
+
+    /**
+     * [addYearbook 新增年鉴页面]
+     * @Author   wangjian
+     * @email    ahtcwangjian@qq.com
+     * @DateTime 2018-05-10
+     */
+    public function addYearbook()
+    {
+    	return $this->fetch();
     }
 }
