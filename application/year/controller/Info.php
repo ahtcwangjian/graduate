40f771d0eae602a04bdd6c8d3b755036e5288715
ahtcwangjian <ahtcwangjian@qq.com>
2018-05-10 13:34:46 +0800

    完成年鉴列表页功能

--- application/year/controller/Info.php
+++ application/year/controller/Info.php
@@ -4,17 +4,29 @@
  * @Author: wangjian
  * @Date:   2018-05-08 22:43:16
  * @Last Modified by:   wangjan
- * @Last Modified time: 2018-05-09 13:37:02
+ * @Last Modified time: 2018-05-10 13:32:51
  * 年鉴信息列表页
  */
 
 namespace app\year\controller;
 use think\Controller;
+use think\Db;
 
 class Info extends Controller
 {
+	/**
+	 * [index 年鉴信息列表页]
+	 * @Author   wangjian
+	 * @email    ahtcwangjian@qq.com
+	 * @DateTime 2018-05-10
+	 * @return   [type]              [description]
+	 */
     public function index()
     {
+    	$data=Db::table('info_copy')->where('key_id','>',0)->column('year');
+        $data=array_unique($data);
+        $this->assign('yeardata',$data);
+        $this->assign('countdata',count($data));
         return $this->fetch();
     }
 }
--- application/year/controller/Manage.php
+++ application/year/controller/Manage.php
@@ -4,11 +4,19 @@
  * @Author: wangjian
  * @Date:   2018-05-08 22:26:09
  * @Last Modified by:   wangjan
- * @Last Modified time: 2018-05-10 10:42:12
+ * @Last Modified time: 2018-05-10 12:58:15
  * 用户操作年鉴信息的界面
  */
 namespace app\year\controller;
 use think\Controller;
+use app\year\model\UserModel;
+use app\year\model\YearbookModel;
+use think\Request;
+use think\Db;
+use think\Session;
+use think\Log;
+use think\Validate;
+use think\File;
 
 class Manage extends Controller
 {
@@ -26,5 +34,41 @@ class Manage extends Controller
     public function addYearbook()
     {
     	return $this->fetch();
+    }
+
+    /**
+     * [saveData 新增年鉴时，提交按钮的点击事件]
+     * @Author   wangjian
+     * @email    ahtcwangjian@qq.com
+     * @DateTime 2018-05-10
+     * @param    Request             $request [description]
+     * @return   [type]                       [description]
+     */
+    public function saveData(Request $request)
+    {
+        $year=$request->param("year");
+        $title=$request->param("title");
+        $author=$request->param("author");
+        $remark=$request->param("remark");
+        $content=$request->param("content");
+        //如果标题 和 内容不为空，则将数据保存到数据库 tk_title  tk_textarea
+        if((!empty($title))&&(!empty($content))){
+
+            //将数据存进数据库
+            $yearbook=new YearbookModel();
+            $yearbook->year=$year;
+            $yearbook->title=$title;
+            $yearbook->author=$author;
+            $yearbook->remark=$remark;
+            $yearbook->textarea=$content;
+            if($yearbook->save()){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }else{
+            return ['data'=>"fail"];
+        }
     }
 }
